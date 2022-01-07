79f98a1bb8b89b6000df72e381e0cdda2ed1aaef
Ottogod <alexandre.kha@yahoo.com>
2022-01-08 00:16:13 +0100

    ook modifié

--- ook.m
+++ ook.m
@@ -1,15 +1,24 @@
 function s = ook(bin, Ts, Fs, fc, Amax)
     nb_motifs = length(bin)
     duree = nb_motifs*Ts
-    t = 0:1/Fs:duree-1/Fs
-    c = Amax*cos(2*pi*fc*t)
-    s = zeros(nb_points,1)'
+    T = 0:1/Fs:duree-1/Fs
+    c = Amax*cos(2*pi*fc*T)
+    s = zeros(length(T),1)'
+    j=1
     for i=1:nb_motifs
-        t = (i-1)*Ts + 1 %tricky les indices
-        if bin(i)==1
-            for j=t:t+Ts -1 %tricky les indices 2
-                s(j)=c(j)
-            end
-        end
+        t = (i-1)*Ts  
+           while T(j)>=t && T(j)<= t+Ts 
+                if bin(i)==1
+                    s(1,j)=c(1,j)
+                end
+                if j== length(T)
+                    break 
+                end
+                if j< length(T)
+                    j=j+1
+                end
+                
+           end
     end
 end
+
