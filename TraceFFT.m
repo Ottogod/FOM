475190a864c57544e0c60c4a440ecd10355d12ee
Ottogod <alexandre.kha@yahoo.com>
2022-01-07 17:29:58 +0100

    first

---
+++ TraceFFT.m
@@ -0,0 +1,14 @@
+function TraceFFT(f0, nb_points, Fs)
+    t = (0:nb_points-1)/Fs
+    x = sin(2*pi*f0*t)
+    Xf = abs(fftshift(fft(x))) 
+    figure
+    plot(t, Xf, 'r') 
+    title('Spectre fréquentiel en amplitude')
+
+end
+
+%{
+fftshift recentre le spectre fréquetiel en zéro. La valeur absolue est là 
+pour permettre d'obtenir une amplitude et non un nombre complexe
+%}
---
+++ ook.m
@@ -0,0 +1,3 @@
+function ook(bin,Ts, Fs, fc, Amax)
+
+end
