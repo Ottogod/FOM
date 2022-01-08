2b0a68cb51b7565494bacdd02371b4f0f3043b58
Ottogod <alexandre.kha@yahoo.com>
2022-01-08 01:33:36 +0100

    fftshift optimal

--- TraceTI.m
+++ TraceTI.m
@@ -1,18 +1,18 @@
 %Q1)
-function TraceTI(f0, duree, Fs)
+function TraceTI(t, x)
+    %{
     t = 0:1/Fs:duree-1/Fs
     x = cos(2*pi*f0*t)
+    %}
     figure
     plot(t,x, 'b') 
     xlabel('t (s)')
-    ylabel('x(t)')
+    ylabel('s(t)')
     ylim([-1.5, 1.5])
-    title("Spectre temporel de x") 
+    title("Spectre temporel") 
 end
 
 %{
-RAS pour cette fonction. Mais doit-on la modifier à chaque fois que l'on
-veut plot une fonction différente ?? On suppose ici qu'elle est de la forme
-sin(2*pi*f0*t)
+RAS pour cette fonction
 %}
    
