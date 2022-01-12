1a713cb1217673f0d1ca9481c2d64c5ec9588901
Ottogod <alexandre.kha@yahoo.com>
2022-01-12 11:38:16 +0100

    SNR rectified to 25

--- Fonctions/Q4_Analyse_spectrale.m
+++ Fonctions/Q4_Analyse_spectrale.m
@@ -1,13 +1,18 @@
 %Analyse spectrale Q4, script test
 
 m = [1, 0, 1]
-fc = 100
+fc = 50
 Amax = 4
 Tb = 0.1
 Fs = 1000
 p = [1, 0, 1, 0, 1]
 s1 = BPSK(m, Tb, fc, Amax, Fs)
 s2 = DSSS_BPSK(m, Tb, fc, Amax, p, Fs)
+
+TraceTI(Time, s1)
+title("Spectre temporel de s1 modulé par BPSK à partir de m [1 0 1]")
+TraceTI(Time, s2)
+title("Spectre temporel de s2 modulé par DSSS à partir de s1")
 
 TraceFFT(Time, s1)
 title("Spectre fréquentiel de s1 modulé par BPSK à partir de m [1 0 1]")
