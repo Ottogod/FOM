cb2f5190c532b108d8a3930580030cc3feacae13
GabrielCMG <gabriel.gueganno@free.fr>
2022-01-13 17:35:59 +0100

    +1

---
+++ Fonctions/Version finale/Analyse_spectrale_DSSS_Q4.m
@@ -0,0 +1,30 @@
+%Analyse spectrale Q4, script test
+
+m_bin = [1, 0, 1];
+p = [1, 0, 1, 0, 1];
+Amax = 4;
+Fc = 50;
+Fs = 1000;
+Tb = 0.1;
+
+s1 = BPSK(m_bin, Amax, Fc, Fs, Tb);
+s2 = DSSS_BPSK(m_bin, p, Amax, Fc, Fs, Tb);
+
+tot_duration = length(m_bin)*Tb;        % Total duration of emitted signal
+t = 0 : 1/Fs : tot_duration - 1/Fs;     % Time vector
+
+figure
+TraceTI(t, s1)
+title("Spectre temporel de s1 modulé par BPSK à partir de m [1 0 1]")
+
+figure
+TraceTI(t, s2)
+title("Spectre temporel de s2 modulé par DSSS à partir de s1")
+
+figure
+TraceFFT(t, s1)
+title("Spectre fréquentiel de s1 modulé par BPSK à partir de m [1 0 1]")
+
+figure
+TraceFFT(t, s2)
+title("Spectre fréquentiel de s2 modulé par DSSS à partir de s1")
